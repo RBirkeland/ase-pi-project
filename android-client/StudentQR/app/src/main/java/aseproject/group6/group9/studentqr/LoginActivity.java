83a2c2fd2f6670ebe7b4946b92d92d6ef38b5771
Florian Schulz <florian.schulz@in.tum.de>
2016-12-21 08:26:59 +0100

    Login with FireBase prototyped - working login interface with change of ui, if user is logged in

--- android-client/StudentQR/app/src/main/java/aseproject/group6/group9/studentqr/LoginActivity.java
+++ android-client/StudentQR/app/src/main/java/aseproject/group6/group9/studentqr/LoginActivity.java
@@ -1,36 +1,175 @@
 package aseproject.group6.group9.studentqr;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.VisibleForTesting;
 import android.support.design.widget.NavigationView;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 /**
  * Created by Florian Schulz on 13.12.2016.
  */
 
-public class LoginActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+public class LoginActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener, View.OnClickListener {
 
     private GoogleApiClient client;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private static final String TAG = "LOGIN";
+    private EditText passwordView;
+    private EditText emailView;
+    private Button loginButtonView;
+    private TextView loginStatusView;
+    private TextView loginAccountNameView;
+    private Button logoutButtonView;
+
+    @VisibleForTesting
+    public ProgressDialog mProgressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.content_login);
 
-        EditText passwordView = (EditText) findViewById(R.id.login_password);
-        EditText emailView = (EditText) findViewById(R.id.login_email);
-        Button loginButtonView = (Button) findViewById(R.id.login_button);
+        passwordView = (EditText) findViewById(R.id.login_password);
+        emailView = (EditText) findViewById(R.id.login_email);
+        loginButtonView = (Button) findViewById(R.id.login_button);
+        loginStatusView = (TextView) findViewById(R.id.login_status_text);
+        loginAccountNameView = (TextView) findViewById(R.id.login_account_name_text);
+        logoutButtonView = (Button) findViewById(R.id.logout_button);
+
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                }
+                updateUI(user);
+            }
+        };
+    }
+
+    @Override
+    public void onClick(View v) {
+        int i = v.getId();
+        if (i == R.id.login_button) {
+            signIn(emailView.getText().toString(), passwordView.getText().toString());
+        } else if (i == R.id.logout_button) {
+            signOut();
+        }
+    }
+
+    private void signIn(String email, String password) {
+        Log.d(TAG, "signIn:" + email);
+        if (!validateForm()) {
+            return;
+        }
+
+        showProgressDialog();
+
+        // [START sign_in_with_email]
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "signInWithEmail:onComplete:" + task.isSuccessful());
+
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            Log.w(TAG, "signInWithEmail:failed", task.getException());
+                            Toast.makeText(LoginActivity.this, R.string.auth_failed,
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+                        // [START_EXCLUDE]
+                        if (!task.isSuccessful()) {
+                            loginStatusView.setText(R.string.auth_failed);
+                        }
+                        hideProgressDialog();
+                        // [END_EXCLUDE]
+                    }
+                });
+        // [END sign_in_with_email]
+    }
+
+    private void signOut() {
+        mAuth.signOut();
+        updateUI(null);
+    }
+
+    private boolean validateForm() {
+        boolean valid = true;
+
+        String email = emailView.getText().toString();
+        if (TextUtils.isEmpty(email)) {
+            emailView.setError(getString(R.string.auth_required));
+            valid = false;
+        } else {
+            emailView.setError(null);
+        }
+
+        String password = passwordView.getText().toString();
+        if (TextUtils.isEmpty(password)) {
+            passwordView.setError(getString(R.string.auth_required));
+            valid = false;
+        } else {
+            passwordView.setError(null);
+        }
+
+        return valid;
+    }
+
+    private void updateUI(FirebaseUser user) {
+        hideProgressDialog();
+        if (user != null) {
+            loginAccountNameView.setText(getString(R.string.login_status_text, user.getEmail()));
+            //mDetailTextView.setText(getString(R.string.firebase_status_fmt, user.getUid()));
+
+            loginButtonView.setVisibility(View.GONE);
+            emailView.setVisibility(View.GONE);
+            passwordView.setVisibility(View.GONE);
+
+            loginStatusView.setVisibility(View.VISIBLE);
+            loginAccountNameView.setVisibility(View.VISIBLE);
+            logoutButtonView.setVisibility(View.VISIBLE);
+
+        } else {
+            loginAccountNameView.setText(null);
+            //mDetailTextView.setText(null)
+
+            loginButtonView.setVisibility(View.VISIBLE);
+            emailView.setVisibility(View.VISIBLE);
+            passwordView.setVisibility(View.VISIBLE);
+
+            loginStatusView.setVisibility(View.GONE);
+            loginAccountNameView.setVisibility(View.GONE);
+            logoutButtonView.setVisibility(View.GONE);
+        }
     }
 
     @Override
@@ -56,10 +195,31 @@ public class LoginActivity extends AppCompatActivity implements NavigationView.O
     @Override
     public void onStart() {
         super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
     }
 
     @Override
     public void onStop() {
         super.onStop();
+        hideProgressDialog();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+
+    public void showProgressDialog() {
+        if (mProgressDialog == null) {
+            mProgressDialog = new ProgressDialog(this);
+            mProgressDialog.setMessage(getString(R.string.progress_loading));
+            mProgressDialog.setIndeterminate(true);
+        }
+
+        mProgressDialog.show();
+    }
+
+    public void hideProgressDialog() {
+        if (mProgressDialog != null && mProgressDialog.isShowing()) {
+            mProgressDialog.dismiss();
+        }
     }
 }
