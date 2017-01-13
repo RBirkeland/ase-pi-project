3849c77b6cf72ce41af2a188b19a9936821d188b
Florian Schulz <florian.schulz@in.tum.de>
2017-01-13 12:25:40 +0100

    install requirements now up to date

--- pi/Project/install-requirements.sh
+++ pi/Project/install-requirements.sh
@@ -1,1 +1,7 @@
-sudo apt-get install fswebcam
+echo "Installing requirements"
+sudo apt-get install fswebcam libzbar0 libzbar-dev
+echo "pip3 python library"
+wget https://bootstrap.pypa.io/get-pip.py
+sudo pip3 install zbarlight --upgrade
+echo "removing not neccesarry stuff"
+rm get-pip.py
